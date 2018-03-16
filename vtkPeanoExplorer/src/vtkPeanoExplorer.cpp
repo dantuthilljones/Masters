51100eb6c9822bd19c6532ac2a11d37cd95fea60
Dan <d.e.tuthill-jones@durham.ac.uk>
2018-03-16 11:53:00 +0000

    Somehow got 3D widgets to work. Committing for reference

--- vtkPeanoExplorer/src/vtkPeanoExplorer.cpp
+++ vtkPeanoExplorer/src/vtkPeanoExplorer.cpp
@@ -1,6 +1,8 @@
 #include "vtkPeanoExplorer.h"
 #include "vtkObjectFactory.h" //for the ::New() macro
 #include "vtkImplicitFunction.h"
+#include "vtkBoxWidget2.h"
+#include "vtkBoxRepresentation.h"
 
 // Macro for the ::New() method
 vtkStandardNewMacro(vtkPeanoExplorer);
@@ -11,5 +13,8 @@ void vtkPeanoExplorer::PrintSelf(ostream& os, vtkIndent indent) {
 }
 
 void vtkPeanoExplorer::SetClipFunction(vtkImplicitFunction* function) {
+    std::cout << "SetClipFunction called!\n";
 
+    vtkBoxWidget2* widge = vtkBoxWidget2::New();
+    widge->CreateDefaultRepresentation();
 }
